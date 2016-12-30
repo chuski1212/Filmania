5d674130b4aefa94b783869a45098b79df37df40
Daniel-OG <daniel.gomez.bellido@est.fib.upc.edu>
2016-12-30 20:40:40 +0100

    RecyclerView casi terminado

---
+++ app/src/main/java/com/example/pr_idi/mydatabaseexample/FilmsAdapter.java
@@ -0,0 +1,86 @@
+package com.example.pr_idi.mydatabaseexample;
+
+import android.content.Context;
+import android.support.v7.widget.*;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by daniel on 30/12/2016.
+ */
+
+public class FilmsAdapter extends RecyclerView.Adapter<FilmsAdapter.ViewHolder>{
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+
+        public TextView pName;
+        public TextView pDirector;
+        public TextView pProtagonist;
+        public TextView pCountry;
+        public TextView pYear;
+        public TextView pRate;
+
+        public ViewHolder(View itemView){
+            super(itemView);
+
+            pName = (TextView) itemView.findViewById(R.id.name);
+            pDirector = (TextView) itemView.findViewById(R.id.director);
+            pProtagonist = (TextView) itemView.findViewById(R.id.protagonist);
+            pCountry = (TextView) itemView.findViewById(R.id.country);
+            pYear = (TextView) itemView.findViewById(R.id.year);
+            pRate = (TextView) itemView.findViewById(R.id.rate);
+
+        }
+    }
+
+    private List<Film> pFilms;
+    private Context pContext;
+
+    public FilmsAdapter(Context context, List<Film> films){
+        pFilms = films;
+        pContext = context;
+    }
+
+    private Context getContext(){
+        return pContext;
+    }
+
+    @Override
+    public FilmsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View filmView = inflater.inflate(R.layout.recyclerview_row, parent, false);
+        ViewHolder viewHolder = new ViewHolder(filmView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(FilmsAdapter.ViewHolder viewHolder, int pos){
+        Film film = pFilms.get(pos);
+        TextView name = viewHolder.pName;
+        name.setText(film.getTitle());
+        TextView director = viewHolder.pDirector;
+        director.setText(film.getDirector());
+        TextView protagonist = viewHolder.pProtagonist;
+        protagonist.setText(film.getProtagonist());
+        TextView country = viewHolder.pCountry;
+        country.setText(film.getCountry());
+        TextView year = viewHolder.pYear;
+        year.setText(film.getYear());
+        TextView rate = viewHolder.pRate;
+        rate.setText(film.getCritics_rate());
+    }
+    @Override
+    public int getItemCount(){
+        return pFilms.size();
+    }
+
+
+}
--- app/src/main/java/com/example/pr_idi/mydatabaseexample/RecyclerView.java
+++ app/src/main/java/com/example/pr_idi/mydatabaseexample/RecyclerView.java
@@ -2,12 +2,21 @@ package com.example.pr_idi.mydatabaseexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+
+import java.util.List;
 
 public class RecyclerView extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view);
+
+        android.support.v7.widget.RecyclerView recyclerFilms = (android.support.v7.widget.RecyclerView) findViewById(R.id.filmview);
+        FilmsAdapter adapter = new FilmsAdapter(this, ((GlobalDBControler) this.getApplication()).getFilmData().getAllFilms());
+        recyclerFilms.setAdapter(adapter);
+        recyclerFilms.setLayoutManager(new LinearLayoutManager(this));
     }
 }
